fb19d2a80e3328aa732dc44d2963466ff0a1a36d
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-12 17:38:24 +0600

    Create iphostname.sh

---
+++ iphostname.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Assign IP Addresses ASK using Shell Script
+bgreen='\033[1;32m'
+red='\033[0;31m'
+nc='\033[0m'
+bold="\033[1m"
+blink="\033[5m"
+echo
+echo
+echo -e "${bgreen}Assign IP Addresses and Hostname,TimeZone Asking using Shell Script ${nc} "
+hostnamectl
+timedatectl
+timedatectl set-timezone Asia/Dhaka
+echo
+read -p "$(echo -e "${bgreen}${bold}${blink}Type System Hostname: ${nc}")" hostname
+hostnamectl set-hostname $hostname
+
+# Creates a backup
+cp /etc/netplan/00-installer-config.yaml /etc/netplan/00-installer-config.yaml.bak_`date +%Y%m%d%H%M`
+ip a
+read -p "Type static IP Interface Name: " STATIC_INTERFACE
+read -p "Type DHCP Interface Name: " DHCP_INTERFACE
+read -p "Type static IP Address with CIDR: " IP_ADDRESS
+read -p "Type Gateway4: " GATEWAY
+read -p "Type DNS: " DNS
+
+cat <<EOF | sudo tee /etc/netplan/00-installer-config.yaml
+network:
+  renderer: networkd
+  ethernets:
+    $STATIC_INTERFACE:
+      dhcp4: no
+      addresses:
+        - $IP_ADDRESS
+      routes: 
+        - to: default
+          via: $GATEWAY
+      nameservers:
+        addresses: [$DNS]
+    $DHCP_INTERFACE:
+      dhcp4: yes
+EOF
+sudo netplan apply
+ip a
