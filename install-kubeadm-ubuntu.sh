ec66800d81538c742ad679b8850907d6dd83a59b
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-18 16:38:55 +0600

    Update install-kubeadm-ubuntu.sh

--- install-kubeadm-ubuntu.sh
+++ install-kubeadm-ubuntu.sh
@@ -7,31 +7,31 @@ bold="\033[1m"
 blink="\033[5m"
 echo
 echo
-
+#
 #Install Kubernetes Using Kubeadm on Ubuntu 22.04.xx
-
+#
 sudo apt update -y && sudo apt upgrade -y
 read -p "$(echo -e "${bgreen}${bold}${blink}Type Worker Hostname: ${nc}")" worker-hostname
 read -p "$(echo -e "${bgreen}${bold}${blink}Type Worker IP Address: ${nc}")" worker-ipaddr
 echo "$worker-ipaddr $worker-hostname.paulco.xyz $worker-hostname" >> /etc/hosts
-
+#
 sudo swapoff –a
 swapoff -a && sed -i '/swap/d' /etc/fstab
-
+#
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
+#
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 sudo sysctl --system
-
+#
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -50,8 +50,8 @@ su - ${USER}
 sudo systemctl enable docker
 systemctl is-active --quiet docker && echo Docker is running
 sudo usermod -aG docker $USER
-
-
+#
+#
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -59,11 +59,11 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install kubelet kubeadm kubectl -y
 sudo apt-mark hold kubelet kubeadm kubectl
-
+#
 systemctl daemon-reload
 systemctl start kubelet
 systemctl enable kubelet.service
-
+#
 kubeadm version
 
 
