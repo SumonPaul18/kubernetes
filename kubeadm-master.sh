aa5d29c0c712a37823becb96183702dad464afd1
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-29 13:51:27 +0600

    Create kubeadm-master.sh

---
+++ kubeadm-master.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Assign IP Addresses ASK using Shell Script
+bgreen='\033[1;32m'
+red='\033[0;31m'
+nc='\033[0m'
+bold="\033[1m"
+blink="\033[5m"
+echo
+echo -e "${bgreen} Configure as Kubeadm Master Node ${nc} "
+echo
+systemctl is-active --quiet kubelet && echo Kubelet is Running
+echo
+kubeadm version
+
+#kubeadm init
+echo
+read -p "$(echo -e "${bgreen}${bold}${blink}TYPE MASTER NODE IP: ${nc}")" MASTERIP
+echo
+read -p "$(echo -e "${bgreen}${bold}${blink}TYPE POD-NETWORK-CIDR Ex-(10.10.0.0/16): ${nc}")" PODCIDR
+sudo kubeadm init --apiserver-advertise-address=$MASTERIP --pod-network-cidr=$PODCIDR
+
+#To start using your cluster, you need to run the following as a regular user:
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#Alternatively, if you are the root user, you can run:
+export KUBECONFIG=/etc/kubernetes/admin.conf
+echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> .bashrc
+
+#Verify Cluster, Nodes and Pods status using kubectl commands
+kubectl cluster-info
+kubectl get nodes
+
+#Configuring Network Plugins
+sudo kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+#Verify To display the network status
+kubectl get pods --all-namespaces
+
+#Joining worker node to a Kubernetes Cluster
+kubeadm token create — print-join-command
+
