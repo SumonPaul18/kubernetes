0b18ed7c4031169a6fd79197e83d9250c708033c
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-29 11:06:13 +0600

    Update install-kubeadm-ubuntu.sh

--- install-kubeadm-ubuntu.sh
+++ install-kubeadm-ubuntu.sh
@@ -52,6 +52,7 @@ systemctl is-active --quiet docker && echo Docker is running
 sudo usermod -aG docker $USER
 containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
 #
 #
 sudo apt-get update
