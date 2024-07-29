67e54c9100938f0daecbb2c7b7358cd86728a865
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-29 11:24:49 +0600

    Update install-kubeadm-ubuntu.sh

--- install-kubeadm-ubuntu.sh
+++ install-kubeadm-ubuntu.sh
@@ -60,7 +60,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install kubelet kubeadm kubectl -y
+sudo apt-get install kubelet kubeadm kubectl -y | tee kubeadm-installation-log.txt
 sudo apt-mark hold kubelet kubeadm kubectl
 #
 systemctl daemon-reload
