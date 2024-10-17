20bf0d30da1ef221140eee0101e17d198f436bb9
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-10-17 12:21:16 +0600

    Update kubeadm-master.sh

--- install-kubeadm/kubeadm-master.sh
+++ install-kubeadm/kubeadm-master.sh
@@ -41,4 +41,4 @@ kubectl get pods --all-namespaces
 
 echo -e "${bgreen} Joining worker node to a Kubernetes Cluster ${nc} "
 #Joining worker node to a Kubernetes Cluster
-kubeadm token create — print-join-command | tee kubeadm-token.txt
+kubeadm token create --print-join-command | tee kubeadm-token.txt
