d8afc1a393fddf6f3678b89e03e5df358159860c
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-10-21 12:35:51 +0600

    skiping shell prompt

---
+++ install-kubeadm/install-k8s-allnode.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+bgreen='\033[1;32m'
+red='\033[0;31m'
+nc='\033[0m'
+bold="\033[1m"
+blink="\033[5m"
+echo
+echo -e "${bgreen}Install Kubeadm on Ubuntu For Master-Worker Nodes${nc} "
+echo
+echo -e "${bgreen}System Information:${nc} "
+hostnamectl
+echo
+echo -e "${bgreen}Time/Date Information:${nc} "
+timedatectl
+echo
+timedatectl set-timezone Asia/Dhaka
+echo
+echo -e "${bgreen}Changed Time Zone:${nc} "
+timedatectl
+echo
+ip a
+#
+#Install Kubernetes Using Kubeadm on Ubuntu 22.04.xx
+read -p "$(echo -e "${bgreen}${bold}${blink}Type System Hostname: ${nc}")" SYSTEMHOSTNAME
+read -p "$(echo -e "${bgreen}${bold}${blink}Type System IP Address: ${nc}")" SYSTEMIP
+hostnamectl set-hostname $SYSTEMHOSTNAME
+echo "$SYSTEMIP $SYSTEMHOSTNAME.paulco.xyz $SYSTEMHOSTNAME" >> /etc/hosts
+echo
+hostname -f
+#
+# Update and upgrade system packages
+DEBIAN_FRONTEND=noninteractive sudo apt update -y && DEBIAN_FRONTEND=noninteractive sudo apt upgrade -y
+#
+sudo swapoff -a
+swapoff -a && sed -i '/swap/d' /etc/fstab
+#
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+#
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+#
+DEBIAN_FRONTEND=noninteractive sudo apt-get update -y
+DEBIAN_FRONTEND=noninteractive sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+docker --version
+sudo usermod -aG docker ${USER}
+#su - ${USER}
+sudo systemctl enable docker
+systemctl is-active --quiet docker && echo Docker is running
+sudo usermod -aG docker $USER
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+#
+#
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install kubelet kubeadm kubectl -y
+sudo apt-mark hold kubelet kubeadm kubectl
+#
+systemctl daemon-reload
+systemctl start kubelet
+systemctl enable kubelet
+#
+kubeadm version
